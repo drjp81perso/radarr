6d6e1374545d382f06f8d8d0c3e5fd154ff56a27
Jean-Paul Lizotte <jean-paul.lizotte@emyode.com>
2024-01-01 00:18:53 -0500

    Last version

--- getradarr.ps1
+++ getradarr.ps1
@@ -6,11 +6,11 @@ Get-ChildItem env:
 write-host("Token is: " + $token)
 start-sleep -Seconds 15
     
-$result = (((Invoke-RestMethod  https://api.github.com/repos/Radarr/Radarr/releases?per_page=10) | Where-Object { $_.prerelease -eq $true })[0].tag_name).replace("v","")
-write-host("Version number is: " + $result)
-$URL = "https://github.com/Radarr/Radarr/releases/download/v$result/Radarr.develop.$result.linux-core-$token.tar.gz"
+#$result = (((Invoke-RestMethod  https://api.github.com/repos/Radarr/Radarr/releases?per_page=10) | Where-Object { $_.prerelease -eq $true })[0].tag_name).replace("v","")
+#write-host("Version number is: " + $result)
+$URL = (((Invoke-RestMethod  https://api.github.com/repos/Radarr/Radarr/releases?per_page=10)))[0].assets.browser_download_url | Where-Object {$_ -ilike "*linux-core-$token*"} 
 Write-Host("Going to try and download from :" + $URL) 
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 5#
 try {
     invoke-webrequest  -uri $URL -UseBasicParsing -OutFile ./Radarr.Binaries.tar.gz
 }
