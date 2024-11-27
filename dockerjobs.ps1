819111421390c4cfcf2068e5a8a2401a72c37600
Jean-Paul Lizotte <36302005+jean-paullizotte@users.noreply.github.com>
2024-11-26 19:58:35 -0500

    fixed some stuff

--- dockerjobs.ps1
+++ dockerjobs.ps1
@@ -3,4 +3,5 @@ docker buildx ./rm mybuilder
 docker buildx create --use --name mybuilder --driver-opt network=host --buildkitd-flags '--allow-insecure-entitlement network.host'
 docker buildx ls
 docker buildx inspect --bootstrap
-docker buildx build --platform=linux/arm64,linux/amd64 -f dockerfile -t drjp81/radarrcore:latest --push --progress=plain .
+docker buildx build --platform=linux/arm64,linux/amd64 -f dockerfile -t drjp81/radarrcore:latest --pull --push .
+##--progress=plain .
--- getradarr.ps1
+++ getradarr.ps1
@@ -9,7 +9,8 @@ start-sleep -Seconds 15
 #$result = (((Invoke-RestMethod  https://api.github.com/repos/Radarr/Radarr/releases?per_page=10) | Where-Object { $_.prerelease -eq $true })[0].tag_name).replace("v","")
 #write-host("Version number is: " + $result)
 $URL = (((Invoke-RestMethod  https://api.github.com/repos/Radarr/Radarr/releases?per_page=10)))[0].assets.browser_download_url | Where-Object {$_ -ilike "*linux-core-$token*"} 
-Write-Host("Going to try and download from :" + $URL) 
+Write-Host("Going to try and download from :" + $URL)
+set-content /work/url.txt -Value $URL
 Start-Sleep -Seconds 5 
 try {
     invoke-webrequest  -uri $URL -UseBasicParsing -OutFile ./Radarr.Binaries.tar.gz
